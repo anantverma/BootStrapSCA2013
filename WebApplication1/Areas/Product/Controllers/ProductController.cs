4a93605e16777245b095d6c961f7d74e5258b6fa
anantverma <dimps.21@gmail.com>
2015-04-01 15:53:54 +0530

    BootStrapSCA2013

---
+++ WebApplication1/Areas/Product/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SPAShoppingCartApp.Areas.Product.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<Models.Product> products;
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private void getProducts()
+        {
+            products = new List<Models.Product>();
+            products.Add(new Models.Product { CategoryId = 1, ProductId = 1, ProductPrice = 1000, ProductName = "Bata Shoes", ProductDescription = "Comfortable shoes.", ProductImage = null });
+            products.Add(new Models.Product { CategoryId = 1, ProductId = 2, ProductPrice = 2000, ProductName = "Bata Shoes", ProductDescription = "Comfortable shoes.", ProductImage = null });
+            products.Add(new Models.Product { CategoryId = 1, ProductId = 3, ProductPrice = 3000, ProductName = "Bata Shoes", ProductDescription = "Comfortable shoes.", ProductImage = null });
+            products.Add(new Models.Product { CategoryId = 1, ProductId = 4, ProductPrice = 4000, ProductName = "Bata Shoes", ProductDescription = "Comfortable shoes.", ProductImage = null });
+            products.Add(new Models.Product { CategoryId = 1, ProductId = 5, ProductPrice = 5000, ProductName = "Bata Shoes", ProductDescription = "Comfortable shoes.", ProductImage = null });
+            products.Add(new Models.Product { CategoryId = 1, ProductId = 6, ProductPrice = 6000, ProductName = "Bata Shoes", ProductDescription = "Comfortable shoes.", ProductImage = null });
+        }
+
+        public ActionResult GetProducts()
+        {
+            getProducts();
+            return View("GetProducts", products);
+        }
+
+        public ActionResult ProductDetails(int id)
+        {
+            getProducts();
+            Models.Product product = products.Where(t => t.ProductId == id).FirstOrDefault();
+            return View("ProductDetails", product);
+        }
+
+    }
+}
---
+++ WebApplication1/Areas/ShoppingCart/Controllers/ShoppingCartController.cs
@@ -0,0 +1,78 @@
+﻿using SPAShoppingCartApp.Areas.ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SPAShoppingCartApp.Areas.ShoppingCart.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        List<CartItem> CartItems;
+        private List<CartItem> GetCartItems()
+        {
+            CartItems = new List<CartItem>();
+            //CartItems.Add(new CartItem { CartId = 1, ProductId = 1, ProductCode = "10001", ProductQuantity = 1, ProductImageSrc = "~/Images/accent.png", ProductName = "Product1", ProductPrice = "1000" });
+            //CartItems.Add(new CartItem { CartId = 1, ProductId = 2, ProductCode = "10002", ProductQuantity = 1, ProductImageSrc = "~/Images/accent.png", ProductName = "Product2", ProductPrice = "2000" });
+            //CartItems.Add(new CartItem { CartId = 1, ProductId = 3, ProductCode = "10003", ProductQuantity = 1, ProductImageSrc = "~/Images/accent.png", ProductName = "Product3", ProductPrice = "3000" });
+            //CartItems.Add(new CartItem { CartId = 1, ProductId = 4, ProductCode = "10004", ProductQuantity = 1, ProductImageSrc = "~/Images/accent.png", ProductName = "Product4", ProductPrice = "4000" });
+            //CartItems.Add(new CartItem { CartId = 1, ProductId = 5, ProductCode = "10005", ProductQuantity = 1, ProductImageSrc = "~/Images/accent.png", ProductName = "Product5", ProductPrice = "5000" });
+            return CartItems;
+        }
+
+        private CartItem CheckIfProductExistsInCart(int productId)
+        {
+            var existingCartItem = (from c in GetCartItemsFromSession()
+                          where c.ProductId == productId
+                          select c).FirstOrDefault();
+            return existingCartItem;
+        }
+
+        public ActionResult CartSummary()
+        {
+            int cartItemsCount = GetCartItemsFromSession().Count;
+            return View("CartSummary", cartItemsCount);
+        }
+
+        public ActionResult AddProductToCart(int productId, int quantity)
+        {
+            var ProductToBeAdded = CheckIfProductExistsInCart(productId);
+            List<CartItem> OldCartItems = GetCartItemsFromSession();
+            if (ProductToBeAdded == null)
+            {
+                OldCartItems.Add(new CartItem { CartId = 1, ProductId = 1, ProductCode = "10001", ProductQuantity = quantity, ProductImageSrc = "~/Images/accent.png", ProductName = "Product1", ProductPrice = "1000" });
+            }
+            else
+            {
+                OldCartItems.Find(c=> c.ProductId == ProductToBeAdded.ProductId).ProductQuantity += quantity;
+            }
+            Session["Cart"] = OldCartItems;
+            return View("Cart", OldCartItems);
+        }
+
+        private List<CartItem> GetCartItemsFromSession()
+        {
+            List<CartItem> OldCartItems;
+            if (Session["Cart"] == null)
+            {
+                GetCartItems();
+                Session["Cart"] = CartItems;
+            }
+            OldCartItems = (List<CartItem>)Session["Cart"];
+            return OldCartItems;
+        }
+
+        public ActionResult Cart()
+        {
+            CartItems = GetCartItemsFromSession();
+            return View("Cart", CartItems);
+        }
+
+        //public ActionResult Delete (int Id)
+        //{
+        //    return View("Cart", CartItems);
+        //}
+
+    }
+}
