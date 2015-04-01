e1f43b6fcfebb738bf08ccba98b13250013b02ce
anantverma <dimps.21@gmail.com>
2015-04-01 22:27:40 +0530

    Modified The layout.

--- WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -21,11 +21,13 @@ namespace SPAShoppingCartApp
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/AppScripts/bootstrap.accordian.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/App/App.css"));
         }
     }
 }
